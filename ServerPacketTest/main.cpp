14b29a6765eef4cb4397995aad06f9b2a790da02
ChrisMiuchiz <chris000005@gmail.com>
2018-11-06 18:01:07 -0500

    Add files via upload

--- ServerPacketTest/main.cpp
+++ ServerPacketTest/main.cpp
@@ -1,21 +1,65 @@
+#undef __STRICT_ANSI__
 #define no_shenanigans __attribute__((noinline)) __declspec(dllexport)
 #include <windows.h>
 #include <iostream>
 #include "callbacks.h"
-
-
+#include "packets.h"
 unsigned int base;
 
-unsigned int __stdcall no_shenanigans HandlePacket(unsigned int packet_id, unsigned int socket_ptr){
-    if (packet_id != 0){
+unsigned int __stdcall no_shenanigans HandlePacket(unsigned int packet_id, SOCKET socket){
+
+//    if (packet_id == 10){
+//        printf("Packet ID: %d\n", packet_id);
+//
+//        unsigned int packet_id2 = 10;
+//        unsigned long long entity_id = 1;
+//        wchar_t msg[1024] = {0};
+//        swprintf(msg, L"Packet ID: %d", packet_id);
+//        unsigned int msgsize = wcslen(msg);
+//
+//        char buf[1024] = {0};
+//        memcpy(buf, &packet_id2, 4);
+//        memcpy(buf + 4, &entity_id, 8);
+//        memcpy(buf + 12, &msgsize, 4);
+//        memcpy(buf + 16, msg, msgsize*2);
+//
+//        AddPacket(socket, buf, 4+8+4+msgsize*2);
+//    }
+
+
+
+    if (packet_id == 1337){
         printf("Packet ID: %d\n", packet_id);
+        printf("Socket: %X\n", socket);
+        unsigned int newpacketid = 1338;
+        //send(socket, (char*)&newpacketid, 4, 0);
+
+        unsigned long long y = 65536;
+        //send(socket, (char*)&y, 8, 0);
+
+        char buf[4+8];
+        memcpy(buf, &newpacketid, 4);
+        memcpy(buf+4, &y, 8);
+        AddPacket(socket, buf, 4+8);
+
+        return 1;
     }
+
     return 0;
 }
+
+
+
+
+void __stdcall no_shenanigans HandleReadyToSend(SOCKET socket){
+    SendQueuedPackets(socket);
+}
+
 
 DWORD WINAPI no_shenanigans RegisterCallbacks(){
 
         RegisterCallback("RegisterPacketCallback", HandlePacket);
+        RegisterCallback("RegisterReadyToSendCallback", HandleReadyToSend);
 
         return 0;
 }
@@ -27,7 +71,8 @@ extern "C" no_shenanigans bool APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwRea
     switch (fdwReason)
     {
         case DLL_PROCESS_ATTACH:
-            HANDLE hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)RegisterCallbacks, 0, 0, NULL);
+            PacketQueueInit();
+            CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)RegisterCallbacks, 0, 0, NULL);
             break;
     }
     return true;
